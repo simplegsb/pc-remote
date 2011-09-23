9817a140339bdc695d68656eab9ee1ea2402eee0
Gary Buyn <simplegsb@gmail.com>
2011-09-24 10:40:42 +1200

    Removed package-info from test packages.

--- com.se.pcremote/src/test/java/com/se/pcremote/client/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * A test suite for the com.se.pcremote.client package.
- * </p>
- */
-package com.se.pcremote.test.client;
--- com.se.pcremote/src/test/java/com/se/pcremote/server/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * A test suite for the com.se.pcremote.server package.
- * </p>
- */
-package com.se.pcremote.test.server;
